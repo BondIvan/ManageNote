5d2155e944e4bec349614156168b11cc559eb6cb
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-10-21 03:22:09 +0200

    Finish method for testing exceptions for class delete

--- src/Tests/CommandsTest/DeleteTest.java
+++ src/Tests/CommandsTest/DeleteTest.java
@@ -1,6 +1,16 @@
 package Tests.CommandsTest;
 
+import Commands.WithParameters.Delete;
+import Entity.NoteEntity;
+import OptionsExceptions.AccessNotFoundException;
+import OptionsExceptions.UnknownArgsException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DeleteTest implements TestCommands {
 
@@ -8,8 +18,16 @@ class DeleteTest implements TestCommands {
     @Test
     public void testForExceptions() {
 
-        
+        List<NoteEntity> notes = new ArrayList<>();
+        Delete delete = new Delete(notes);
 
+        Exception unknownArgsException1 = assertThrows(UnknownArgsException.class, () -> delete.perform(""));
+        Exception unknownArgsException2 = assertThrows(UnknownArgsException.class, () -> delete.perform("arg1_name arg2_andMore"));
+        Exception accessNotFoundException = assertThrows(AccessNotFoundException.class, () -> delete.perform("arg1_name"));
+
+        Assertions.assertEquals("Нет параметров", unknownArgsException1.getMessage());
+        Assertions.assertEquals("Параметров больше чем нужно", unknownArgsException2.getMessage());
+        Assertions.assertEquals("Сервис не найден", accessNotFoundException.getMessage());
     }
 
 }
