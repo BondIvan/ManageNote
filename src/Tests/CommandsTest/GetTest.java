adbea64fdfac59b091981c7f02c62643504e3893
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-10-23 03:26:26 +0200

    Add a test for obtaining a service account by login

--- src/Tests/CommandsTest/GetTest.java
+++ src/Tests/CommandsTest/GetTest.java
@@ -23,6 +23,58 @@ class GetTest implements TestCommands {
      *
      ***/
 
+    //TODO Добавить тест на получение сервиса по логину
+
+    @Test
+    void testGetByLogin() throws UnknownArgsException, AccessNotFoundException {
+
+        List<NoteEntity> notes = new ArrayList<>();
+        NoteEntity note1 = new NoteEntity("Vk.com (1-st account)", "first.account@gmail.com"); note1.setPassword("password_vk_1");
+        NoteEntity note2 = new NoteEntity("Vk.com (2-nd account)", "second.account@gmail.com"); note2.setPassword("password_vk_2");
+        NoteEntity note3 = new NoteEntity("Vk.com (3-rd account)", "third.account@gmail.com"); note3.setPassword("password_vk_3");
+        NoteEntity note4 = new NoteEntity("Vk.com (4-th account)", "fourth.account@gmail.com"); note4.setPassword("password_vk_4");
+        notes.add(note1);
+        notes.add(note2);
+        notes.add(note3);
+        notes.add(note4);
+
+        String login1 = "first.account@gmail.com";
+        String login3 = "third.account@gmail.com";
+        String login_no = "no.account@gmail.com";
+
+        NoteEntity noteEntity1 = notes.stream()
+                .filter(note -> note.getIdService().split(" ")[0].equalsIgnoreCase("vk.com")) // Сравнивается первое слово текущего сервиса с требуемым
+                .filter(note -> note.getLogin().equalsIgnoreCase(login1))
+                .findFirst()
+                .orElseThrow(() -> new AccessNotFoundException("Сервис не найден"));
+
+        NoteEntity noteEntity3 = notes.stream()
+                .filter(note -> note.getIdService().split(" ")[0].equalsIgnoreCase("vk.com"))
+                .filter(note -> note.getLogin().equalsIgnoreCase(login3))
+                .findFirst()
+                .orElseThrow(() -> new AccessNotFoundException("Сервис не найден"));
+
+        Exception noteEntity_noByLogin = assertThrows(AccessNotFoundException.class, () ->
+                        notes.stream()
+                        .filter(note -> note.getIdService().split(" ")[0].equalsIgnoreCase("vk.com"))
+                        .filter(note -> note.getLogin().equalsIgnoreCase(login_no))
+                        .findFirst()
+                        .orElseThrow(() -> new AccessNotFoundException("Сервис не найден")));
+
+        Exception noteEntity_noByServiceName = assertThrows(AccessNotFoundException.class, () ->
+                notes.stream()
+                        .filter(note -> note.getIdService().split(" ")[0].equalsIgnoreCase("kvvvv.com"))
+                        .filter(note -> note.getLogin().equalsIgnoreCase(login3))
+                        .findFirst()
+                        .orElseThrow(() -> new AccessNotFoundException("Сервис не найден")));
+
+        Assertions.assertEquals(note1, noteEntity1);
+        Assertions.assertEquals(note3, noteEntity3);
+
+        Assertions.assertEquals("Сервис не найден", noteEntity_noByLogin.getMessage());
+        Assertions.assertEquals("Сервис не найден", noteEntity_noByServiceName.getMessage());
+    }
+
     @Test
     void testIgnoreCase() throws IOException, AccessNotFoundException, UnknownArgsException {
 
