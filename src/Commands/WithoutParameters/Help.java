41ed7dd30aad2fe848b2e39832442065e2b8a0df
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-11-07 23:33:29 +0100

    Update command help

--- src/Commands/WithoutParameters/Help.java
+++ src/Commands/WithoutParameters/Help.java
@@ -10,9 +10,9 @@ public class Help implements Commands {
     @Override
     public String perform(String postfix) {
         return """
-                        For get access: -get- [название]  [true] - получить похожие
-                        For get names of all services: -getall-
-                        For add new access: -add- [название] [login] [password]
+                        Get access: -get- [название]
+                        Get all services: -getall- [] / [date]
+                        Add new access: -add- [название] [login] [password]
                         Replace access: -replace- [название] [service/login/password] [новая строка]
                         Delete access: -delete- [название]
                         Save file: -save-
