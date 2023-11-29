11d9bc24ac51d3187b9ca35dfd6beb435bde6b22
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-11-29 04:55:27 +0300

    Update test method for exceptions for class ReplaceTest

--- src/Tests/CommandsTest/ReplaceTest.java
+++ src/Tests/CommandsTest/ReplaceTest.java
@@ -39,8 +39,6 @@ class ReplaceTest implements TestCommands {
         String postfix2 = "logo.com service Logo.com";
         System.out.println( replace.perform(postfix2) );
 
-        //TODO тест на переименовывание названия само на себя у сервиса без аккаунтов
-
         Assertions.assertEquals("Logo.com", note4.getIdService());
 
         // Изменить название сервиса без аккаунтов на сервис также без аккаунтов
@@ -122,7 +120,7 @@ class ReplaceTest implements TestCommands {
     }
 
     @Test
-    void testReplaceServiceLogin_withAccounts() throws UnknownArgsException, AccessNotFoundException, IncorrectValueException {
+    void testReplaceServiceLogin_withAccounts() throws UnknownArgsException, AccessNotFoundException {
 
         List<NoteEntity> notes = new ArrayList<>();
         NoteEntity note1 = new NoteEntity("Vk.com (1-st account)", "first.account@gmail.com"); note1.setPassword("password_vk_1");
@@ -155,7 +153,7 @@ class ReplaceTest implements TestCommands {
     }
 
     @Test
-    void testReplaceServicePassword_withAccounts() throws UnknownArgsException, AccessNotFoundException, IncorrectValueException {
+    void testReplaceServicePassword_withAccounts() throws UnknownArgsException, AccessNotFoundException {
 
         List<NoteEntity> notes = new ArrayList<>();
         NoteEntity note1 = new NoteEntity("Vk.com (1-st account)", "first.account@gmail.com"); note1.setPassword("password_vk_1");
@@ -178,28 +176,33 @@ class ReplaceTest implements TestCommands {
         List<NoteEntity> notes = new ArrayList<>();
         Replace replace = new Replace(notes);
 
-        NoteEntity note1 = new NoteEntity("Vk.com", "first.account@gmail.com");
+        NoteEntity note1 = new NoteEntity("Vk.com (1-st account)", "first.account@gmail.com");
         NoteEntity note2 = new NoteEntity("Telegram.com", "first.account@gmail.com");
-        notes.add(note1);
-        notes.add(note2);
+        NoteEntity note3 = new NoteEntity("Vk.com (2-nd account)", "second.account@gmail.com");
+        notes.add(note1); notes.add(note2); notes.add(note3);
         try {
             note1.setPassword("password_vk_1");
             note2.setPassword("password_telegram_2");
+            note3.setPassword("password_vk_2");
         } catch (UnknownArgsException e) {
             System.err.println(e.getMessage());
         }
 
-        Exception accessNotFoundException = assertThrows(AccessNotFoundException.class, () -> replace.perform("arg1_name service arg3_newString"));
+        Exception accessNotFoundException1 = assertThrows(AccessNotFoundException.class, () -> replace.perform("arg1_name service arg3_newString"));
+        Exception accessNotFoundException2 = assertThrows(AccessNotFoundException.class, () -> replace.perform("vk.com wrongLogin service arg3_newString"));
         Exception unknownArgsException1 = assertThrows(UnknownArgsException.class, () -> replace.perform("arg1_name arg2_typeAndLess"));
-        Exception unknownArgsException2 = assertThrows(UnknownArgsException.class, () -> replace.perform("arg1_name login arg3_newString arg4_andMore"));
+        Exception unknownArgsException2 = assertThrows(UnknownArgsException.class, () -> replace.perform("arg1_name args2_login args3_type arg4_newString andMore"));
         Exception unknownArgsException3 = assertThrows(UnknownArgsException.class, () -> replace.perform("Vk.com arg2_wrongType arg3_newString"));
-        Exception incorrectValueException1 = assertThrows(IncorrectValueException.class, () -> replace.perform("Vk.com login first.account@gmail.com"));
+        Exception unknownArgsException4 = assertThrows(UnknownArgsException.class, () -> replace.perform("Vk.com args_login arg2_wrongType arg3_newString"));
+        Exception incorrectValueException1 = assertThrows(IncorrectValueException.class, () -> replace.perform("Vk.com second.account@gmail.com login first.account@gmail.com"));
         Exception incorrectValueException2 = assertThrows(IncorrectValueException.class, () -> replace.perform("Telegram.com service Vk.com"));
 
-        Assertions.assertEquals("Сервис не найден", accessNotFoundException.getMessage());
+        Assertions.assertEquals("Сервис не найден", accessNotFoundException1.getMessage());
+        Assertions.assertEquals("Неправильный логин аккаунта", accessNotFoundException2.getMessage());
         Assertions.assertEquals("Параметров меньше чем нужно", unknownArgsException1.getMessage());
         Assertions.assertEquals("Параметров больше чем нужно", unknownArgsException2.getMessage());
         Assertions.assertEquals("Неизвестный параметр", unknownArgsException3.getMessage());
+        Assertions.assertEquals("Неизвестный параметр", unknownArgsException4.getMessage());
         Assertions.assertEquals("У этого сервиса такой логин уже существует", incorrectValueException1.getMessage()); //Если менять только логин
         Assertions.assertEquals("Такой логин уже есть", incorrectValueException2.getMessage()); //Если менять только логин
     }
