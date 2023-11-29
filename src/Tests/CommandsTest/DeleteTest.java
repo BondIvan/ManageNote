acc7291f54f3b05149c982e6a5b7678cdb6b1f18
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-11-29 04:16:27 +0300

    Update tests for Delete class

--- src/Tests/CommandsTest/DeleteTest.java
+++ src/Tests/CommandsTest/DeleteTest.java
@@ -10,65 +10,83 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DeleteTest implements TestCommands {
 
     @Test
-    void testServiceWithoutAccount() throws UnknownArgsException, AccessNotFoundException {
-
-        List<NoteEntity> notes = new ArrayList<>();
-        NoteEntity note = new NoteEntity("Vk.com", "first.account@gmail.com"); note.setPassword("password_vk_1");
-        notes.add(note);
-
-        Delete delete = new Delete(notes);
-        System.out.println( delete.perform("Vk.com") );
-
-        boolean isDeleted = notes.stream()
-                .anyMatch(note1 -> note1.getIdService().equalsIgnoreCase("Vk.com"));
-
-        Assertions.assertFalse(isDeleted);
-    }
-
-    @Test
-    void testServiceWithTwoAccount() throws UnknownArgsException, AccessNotFoundException {
+    void testDelete_perform() throws UnknownArgsException, AccessNotFoundException {
 
         List<NoteEntity> notes = new ArrayList<>();
         NoteEntity note1 = new NoteEntity("Vk.com (1-st account)", "first.account@gmail.com"); note1.setPassword("password_vk_1");
         NoteEntity note2 = new NoteEntity("Vk.com (2-nd account)", "second.account@gmail.com"); note2.setPassword("password_vk_2");
-        notes.add(note1);
-        notes.add(note2);
+        NoteEntity note3 = new NoteEntity("Telegram.com", "teleg.account@gmail.com"); note3.setPassword("password_teleg_1");
+        NoteEntity note4 = new NoteEntity("logo.com", "logo.account@gmail.com"); note4.setPassword("password_logo_1");
+        NoteEntity note5 = new NoteEntity("Yandex.ru (1-st account)", "yandex1.account@gmail.com"); note5.setPassword("password_yandex_1");
+        NoteEntity note6 = new NoteEntity("Yandex.ru (2-nd account)", "yandex2.account@gmail.com"); note6.setPassword("password_yandex_2");
+        NoteEntity note7 = new NoteEntity("Yandex.ru (3-rd account)", "yandex3.account@gmail.com"); note7.setPassword("password_yandex_3");
+        notes.add(note1); notes.add(note2); notes.add(note3); notes.add(note4); notes.add(note5); notes.add(note6); notes.add(note7);
 
-        String postfixWithName = "vk.com";
+        Delete delete = new Delete(notes);
 
-        // В автоматических тестах не принято использовать какое-то взаимодействие с пользователем.
-        // Поэтому в этом тесте используется такой же порядок действий как и в классе Delete.
-        // Грубо говоря, здесь тестируются методы из класса UsefulMethods
-        // TODO В будещем нужно будет изменить этот тест
+        String postfix1 = "telegram.com";
+        String actual1 = delete.perform(postfix1);
+        boolean isDeleted1 = notes.stream().anyMatch(note -> note.getIdService().equals("Telegram.com"));
+        Assertions.assertFalse(isDeleted1);
+        Assertions.assertEquals("Удалено", actual1);
 
-        String wrongLogin = "wrong.login@gmail.com";
-        Exception accessNotFoundException = assertThrows(AccessNotFoundException.class,
-                () -> UsefulMethods.getAccountFromServiceByLogin(notes, postfixWithName, wrongLogin));
+        String postfix2 = "vk.com first.account@gmail.com";
+        String actual2 = delete.perform(postfix2);
+        boolean isDeleted2 = notes.stream().anyMatch(note -> note.getIdService().equals("Vk.com (1-st account)"));
+        Assertions.assertFalse(isDeleted2);
+        Assertions.assertEquals("Удалено", actual2);
+        Assertions.assertEquals("Vk.com", note2.getIdService());
 
-        // Проверка на ошибку неверного ввода логина аккаунта
-        Assertions.assertEquals("Такой записи нет", accessNotFoundException.getMessage());
+        String postfix3 = "logo.com logo.account@gmail.com";
+        String actual3 = delete.perform(postfix3);
+        boolean isDeleted3 = notes.stream().anyMatch(note -> note.getIdService().equals("logo.com"));
+        Assertions.assertFalse(isDeleted3);
+        Assertions.assertEquals("Удалено", actual3);
 
-        String deletedLogin = "second.account@gmail.com";
-        NoteEntity deletedNote = UsefulMethods.getAccountFromServiceByLogin(notes, postfixWithName, deletedLogin); // Выбор сервиса по логину
-        notes.remove(deletedNote); // Удаление сервиса аналогично как в классе Delete
-        UsefulMethods.changingNameWhenRemove(notes, postfixWithName); // Изменение названия сервиса в соответствии с оставшимися у него аккаунтами
+        String postfix4 = "yandex.ru";
+        String actual4 = delete.perform(postfix4);
+        List<NoteEntity> yandexList = notes.stream()
+                .filter(note -> note.getIdService().contains("Yandex.ru"))
+                .collect(Collectors.toList());
 
-        // Удалён второй аккаунт
-        boolean deletedSecondAcc = notes.contains(note2);
-        Assertions.assertFalse(deletedSecondAcc);
+        Assertions.assertEquals(List.of(note5, note6, note7), yandexList);
+        Assertions.assertEquals("Теперь введите команду", actual4);
+    }
 
-        // У оставшегося сервиса удалён номер ("1-st account")
-        boolean remainingAcc = notes.stream()
-                        .anyMatch(note -> note.getIdService().equalsIgnoreCase("vk.com"));
+    @Test
+    void testDelete_deleteNoteByLogin() throws UnknownArgsException, AccessNotFoundException {
 
-        Assertions.assertEquals("Vk.com", note1.getIdService());
-        Assertions.assertTrue(remainingAcc);
+        List<NoteEntity> notes = new ArrayList<>();
+        NoteEntity note1 = new NoteEntity("Vk.com (1-st account)", "first.account@gmail.com"); note1.setPassword("password_vk_1");
+        NoteEntity note2 = new NoteEntity("Vk.com (2-nd account)", "second.account@gmail.com"); note2.setPassword("password_vk_2");
+        NoteEntity note3 = new NoteEntity("Telegram.com", "teleg.account@gmail.com"); note3.setPassword("password_teleg_1");
+        NoteEntity note4 = new NoteEntity("logo.com", "logo.account@gmail.com"); note4.setPassword("password_logo_1");
+        NoteEntity note5 = new NoteEntity("Yandex.ru", "yandex.account@gmail.com"); note5.setPassword("password_yandex_1");
+        notes.add(note1); notes.add(note2); notes.add(note3); notes.add(note4); notes.add(note5);
+
+        Delete delete = new Delete(notes);
+
+        String[] args1 = UsefulMethods.makeArgsTrue("vk.com second.account@gmail.com"); // Сервис с аккаунтами
+        String[] args2 = UsefulMethods.makeArgsTrue("telegram.com teleg.account@gmail.com"); // Сервис без аккаунтов
+
+        String actual1 = delete.deleteNoteByLogin(args1[0], args1[1]);
+        String actual2 = delete.deleteNoteByLogin(args2[0], args2[1]);
+
+        boolean isDeleted1 = notes.stream().anyMatch(note -> note.getIdService().equals("Vk.com (2-nd account)"));
+        boolean isDeleted2 = notes.stream().anyMatch(note -> note.getIdService().equals("telegram.com"));
+
+        Assertions.assertFalse(isDeleted1);
+        Assertions.assertFalse(isDeleted2);
+
+        Assertions.assertEquals("Удалено", actual1);
+        Assertions.assertEquals("Удалено", actual2);
     }
 
     @Test
