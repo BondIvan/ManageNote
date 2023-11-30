4a4676e7778725862e56d9c38a031eddaec204a9
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-11-30 05:01:20 +0300

    Add command processing get to the bot

--- src/Telegram/BotForBackup/MyBkBot.java
+++ src/Telegram/BotForBackup/MyBkBot.java
@@ -1,26 +1,21 @@
-package Telegram.Bot;
+package Telegram.BotForBackup;
 
+import Commands.WithParameters.Get;
+import Entity.NoteEntity;
 import OptionsExceptions.AccessNotFoundException;
-import OptionsExceptions.UnknownArgsException;
-import Source.StartConsole;
+import Telegram.Commands.SendMsg;
+import Tools.UsefulMethods;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
-public class BotWithBackups extends TelegramLongPollingBot {
+public class MyBkBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
-        //TODO Сделать другую фабрику, которая будет отвечать чисто за команды для бота
 
         if(!update.hasMessage() || !update.getMessage().hasText())
             return;
@@ -31,70 +26,37 @@ public class BotWithBackups extends TelegramLongPollingBot {
         String prefix = message.split(" ")[0]; // Введённая команда
         String postfix = message.substring(prefix.length()).trim(); // Аргументы введённой команды
 
-        BotGet botGet = new BotGet();
+        if(!prefix.equalsIgnoreCase("get"))
+            return;
 
         try {
 
-            //Commands command = commandFactory.getCommand(prefix);
-            if(prefix.equals("get"))
-                sendMessage(chatId, botGet.perform(postfix));
+            for(NoteEntity nt: get(postfix, chatId))
+                execute( SendMsg.sendMessage(chatId, nt.toString()) );
 
-        } catch (AccessNotFoundException | UnknownArgsException e) {
+        } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    public void sendMessage(long chatId, String text) {
+    public List<NoteEntity> get(String postfix, long chatId) throws TelegramApiException {
 
-        int placeLastSpace = text.lastIndexOf(" ");
-        int amongCharacterInPassword = text.length() - placeLastSpace;
-
-        MessageEntity messageEntity = new MessageEntity();
-        messageEntity.setType("spoiler");
-        messageEntity.setOffset(placeLastSpace); // С какого символа начинать скрывать
-        messageEntity.setLength(amongCharacterInPassword); // На сколько символов скрывать
-
-        List<MessageEntity> messageEntityList = new ArrayList<>();
-        messageEntityList.add(messageEntity);
-
-        String idChat = String.valueOf(chatId);
-        SendMessage sendMessage = new SendMessage(idChat, text);
-        sendMessage.disableNotification();
-
-        sendMessage.setEntities(messageEntityList);
+        String[] args = UsefulMethods.makeArgsTrue(postfix);
 
         try {
+            Get get = new Get();
+            if(args.length > 1) {
+                return List.of( get.getNoteByLogin(args[0], args[1]) );
+            }
+            return get.getListWithNotes(args[0]);
+        } catch (AccessNotFoundException e) {
+            System.out.println("Ошибка get в bkBot: " + e.getMessage());
+
+            SendMessage sendMessage = new SendMessage(String.valueOf(chatId), e.getMessage());
             execute(sendMessage);
-        } catch (TelegramApiException e) {
-            System.out.println(e.getMessage());
         }
 
-    }
-
-    public void sendFile(long chatId) {
-
-        InputFile inputFile = new InputFile();
-
-        File file = new File(StartConsole.PATH);
-        inputFile.setMedia(file);
-
-        SendDocument sendDocument = new SendDocument();
-        sendDocument.setDocument(inputFile.setMedia(file));
-        sendDocument.setChatId(chatId);
-        sendDocument.getProtectContent();
-
-        try {
-            execute(sendDocument);
-        } catch (TelegramApiException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    // Идентификационный номер чата с ботом
-    public long getChatID() {
-        return 347329462;
+        throw new TelegramApiException("Странные данные");
     }
 
     @Override
