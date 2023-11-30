653a6e87d6f70e0e285b45e72f77674da930064f
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2023-11-30 04:58:23 +0300

    Split the send file and send message commands into different classes

--- src/Telegram/Commands/SendFile.java
+++ src/Telegram/Commands/SendFile.java
@@ -1,2 +1,25 @@
-package Telegram.Commands;public class SendFile {
+package Telegram.Commands;
+
+import Source.StartConsole;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+
+import java.io.File;
+
+public class SendFile {
+
+    public SendDocument sendFile(long chatId) {
+        InputFile inputFile = new InputFile();
+
+        File file = new File(StartConsole.PATH);
+        inputFile.setMedia(file);
+
+        SendDocument sendDocument = new SendDocument();
+        sendDocument.setDocument(inputFile.setMedia(file));
+        sendDocument.setChatId(chatId);
+        sendDocument.getProtectContent();
+
+        return sendDocument;
+    }
+
 }
--- src/Telegram/Commands/SendMsg.java
+++ src/Telegram/Commands/SendMsg.java
@@ -1,2 +1,33 @@
-package Telegram.Commands;public class SendMessage {
+package Telegram.Commands;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SendMsg {
+
+    public static SendMessage sendMessage(long chatId, String text) {
+
+        int placeLastSpace = text.lastIndexOf(" ");
+        int amongCharacterInPassword = text.length() - placeLastSpace;
+
+        MessageEntity messageEntity = new MessageEntity();
+        messageEntity.setType("spoiler");
+        messageEntity.setOffset(placeLastSpace); // С какого символа начинать скрывать
+        messageEntity.setLength(amongCharacterInPassword); // На сколько символов скрывать
+
+        List<MessageEntity> messageEntityList = new ArrayList<>();
+        messageEntityList.add(messageEntity);
+
+        String idChat = String.valueOf(chatId);
+        SendMessage sendMessage = new SendMessage(idChat, text);
+        sendMessage.disableNotification();
+
+        sendMessage.setEntities(messageEntityList);
+
+        return sendMessage;
+    }
+
 }
